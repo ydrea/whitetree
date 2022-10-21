4544972a04f73e7d11fc6d26b6878061253394bf
ydrea <ydrea.wrld@gmail.com>
2022-10-21 16:32:23 +0200

    baby steps

--- blog/pages/[slug].js
+++ blog/pages/[slug].js
@@ -1,24 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
-
 //how many?
 export async function getStaticPaths () {
     const res = await fetch('http://localhost:1337/api/restaurants')
     const restaurants = await res.json()
     const items = restaurants.data
-    console.log('====================================');
-    console.log(items);
-    console.log('====================================');
     const paths = items.map((i)=>({
-        params: {slug: i.Slug}
+        params: {slug: i.attributes.Slug}
     }))
 return { paths: [], fallback: false}  
 }
 // with what?
 export async function getStaticProps ({params}) {
-    console.log('====================================');
-    console.log(params);
-    console.log('====================================');
     const {slug} = params
+    console.log(slug);
     const res = await fetch(`http://localhost:1337/api/restaurants?Slug=${slug}`)
 const data =  await res.json()
 const restaurant = data[0]
@@ -29,9 +24,10 @@ export default function Restaurant({restaurant}) {
  
   return (
     <div>
-      {
-      JSON.stringify( restaurant
-      )
-      }</div>
+      <Link href='/'>
+        <a>&rarrw;</a>
+      </Link>
+      {restaurant.ime}      
+      </div>
   )
 }
