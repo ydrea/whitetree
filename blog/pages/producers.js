162db61e8b8e2e9a11bf51ce51b0362aff87142e
ydrea <ydrea.wrld@gmail.com>
2022-10-29 19:21:40 +0200

    4 streams of data.items

--- blog/pages/producers.js
+++ blog/pages/producers.js
@@ -1,5 +1,7 @@
-import Layout from "../components/Layout"
-//
+
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import styles from "../styles/Home.module.c//";
 export async function getStaticProps() {
     const res = await fetch(`${process.env.BASE_URL}/producers`)
     const resProducers = await res.json()
@@ -12,8 +14,17 @@ export default function Producers({items}){
     return (
         <Layout>
             <div>
-                <span>
-                    {JSON.stringify(items)}
+                <span>    
+        {items && items.map((i) => (
+          <Link  href={`/producer/${i.id}`} key={i.id}>
+          <a className={styles.card}>
+
+          <h2>{i.attributes.name} &rArr;</h2>
+          {i.attributes.description}
+          </a>
+          </Link>)
+        )}
+ 
                 </span>
             </div>
         </Layout>
