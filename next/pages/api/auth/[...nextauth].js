416c7840acf077de11a031a7ce5832c2c63daabf
ydrea <ydrea.wrld@gmail.com>
2022-11-07 17:13:51 +0100

    nesto radi...

--- next/pages/api/auth/[...nextauth].js
+++ next/pages/api/auth/[...nextauth].js
@@ -1,64 +1,99 @@
 import NextAuth from "next-auth";
-import FacebookProvider from "next-auth/providers/facebook";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import TwitterProvider from "next-auth/providers/twitter";
-// import AppleProvider from "next-auth/providers/apple"
+// import GithubProvider from "next-auth/providers/github";
+// import FacebookProvider from "next-auth/providers/facebook";
+// import TwitterProvider from "next-auth/providers/twitter";
 // import EmailProvider from "next-auth/providers/email"
+import GoogleProvider from "next-auth/providers/google";
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
-export const authOptions = {
-  // https://next-auth.js.org/configuration/providers/oauth
+const options = {
   providers: [
-    /* EmailProvider({
-         server: process.env.EMAIL_SERVER,
-         from: process.env.EMAIL_FROM,
-       }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    */
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_CONSUMER_KEY,
-      clientSecret: process.env.TWITTER_CONSUMER_SECRET,
-    }),
-    // Auth0Provider({
-    //   clientId: process.env.AUTH0_ID,
-    //   clientSecret: process.env.AUTH0_SECRET,
-    //   issuer: process.env.AUTH0_ISSUER,
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET,
     // }),
   ],
-  theme: {
-    colorScheme: "light",
+
+  secret: process.env.NEXT_PUBLIC_SECRET,
+  session: {
+    jwt: true,
   },
   callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin";
+    async session({ session, token, user }) {
+      session.jwt = token.jwt;
+      session.id = token.id;
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      if (user) {
+        const response = await fetch(
+          `${process.env.NEXTAUTH_URL}/api/auth/google/callback?access_token=${account?.access_token}`
+          // {
+          //   method: "GET",
+          //   headers: {
+          //     // update with your user-agent
+          //     // "User-Agent":
+          //     //   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+          //     Accept: "application/json; charset=UTF-8",
+          //   },
+          // }
+        );
+        // const responser = JSON.parse();
+        const data = await response.text();
+        console.log(data);
+        // token.jwt = data.jwt;
+        // token.id = data.user.id;
+      }
       return token;
     },
   },
 };
 
-export default NextAuth(authOptions);
+const Auth = (req, res) => NextAuth(req, res, options);
+
+export default Auth;
+
+//
+
+// For more information on each option (and a full list of options) go to
+// // https://next-auth.js.org/configuration/options
+// export const authOptions = {
+//   // https://next-auth.js.org/configuration/providers/oauth
+//   providers: [
+//     /* EmailProvider({
+//          server: process.env.EMAIL_SERVER,
+//          from: process.env.EMAIL_FROM,
+//        }),
+//     */
+//     FacebookProvider({
+//       clientId: process.env.FACEBOOK_ID,
+//       clientSecret: process.env.FACEBOOK_SECRET,
+//     }),
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
+//     TwitterProvider({
+//       clientId: process.env.TWITTER_CONSUMER_KEY,
+//       clientSecret: process.env.TWITTER_CONSUMER_SECRET,
+//     }),
+//   ],
+//   theme: {
+//     colorScheme: "light",
+//   },
+//   callbacks: {
+//     async jwt({ token }) {
+//       token.userRole = "admin";
+//       return token;
+//     },
+//   },
+// };
+
+// export default NextAuth(authOptions);
---
+++ str-api/config/plugins.js
@@ -0,0 +1,8 @@
+module.exports = {
+  default: {},
+  validator() {},
+
+  "strapi-google-auth": {
+    enabled: true,
+  },
+};
