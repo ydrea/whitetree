a13900a2d48abf2bf5b224ac921e1d0719e20f5c
ydrea <ydrea.wrld@gmail.com>
2022-11-15 17:49:15 +0100

    products

--- next/context/AuthContext.js
+++ next/context/AuthContext.js
@@ -2,64 +2,88 @@ import { Magic } from "magic-sdk";
 import { useRouter } from "next/router";
 import { createContext, useEffect, useState } from "react";
 import { MAGIC_PUBLIC_KEY } from "../utils/urls";
-//
-const AuthContext = createContext();
-//
-let magic;
-//
-export const AuthProvider = (props) => {
-  const [user, userSet] = useState(null);
-  const router = useRouter();
 
+const AuthContext = createContext();
+
+let magic;
+
+export const AuthProvider = (props) => {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+  //
+  /**
+   * Log the user in
+   * @param {string} email
+   */
   const loginUser = async (email) => {
     try {
       await magic.auth.loginWithMagicLink({ email });
-      userSet({ email });
+      setUser({ email });
       router.push("/");
-    } catch (error) {
-      userSet(null);
+    } catch (err) {
+      console.log(err);
     }
   };
 
+  /**
+   * Log the user out
+   */
   const logoutUser = async () => {
     try {
       await magic.user.logout();
-      userSet(null);
+      setUser(null);
       router.push("/");
-    } catch (error) {
-      console.log("logoutUser error", error);
+    } catch (err) {
+      console.log(err);
     }
   };
 
+  /**
+   * If user is logged in, get data and display it
+   */
+  const checkUserLoggedIn = async () => {
+    try {
+      const isLoggedIn = await magic.user.isLoggedIn();
+
+      if (isLoggedIn) {
+        const { email } = await magic.user.getMetadata();
+        setUser({ email });
+        //Add this just for test
+        const token = await getToken();
+        console.log("checkUserLoggedIn token", token);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  /**
+   * Retrieve Magic Issued Bearer Token
+   * This allows User to make authenticated requests
+   */
   const getToken = async () => {
     try {
       const token = await magic.user.getIdToken();
       return token;
-    } catch (error) {}
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const persistSession = async () => {
-    try {
-      const isLoggedIn = magic.user.isLoggedIn();
-      if (isLoggedIn) {
-        const { email } = await magic.user.getMetaData();
-        userSet({ email });
-
-        const token = getToken();
-        console.log("token", token);
-      }
-    } catch (error) {}
-  };
-
+  /**
+   * Reload user login on app refresh
+   */
   useEffect(() => {
     magic = new Magic(MAGIC_PUBLIC_KEY);
-    persistSession();
+
+    checkUserLoggedIn();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, loginUser, logoutUser, getToken }}>
+    <AuthContext.Provider value={{ user, logoutUser, loginUser, getToken }}>
       {props.children}
     </AuthContext.Provider>
   );
 };
+
 export default AuthContext;
