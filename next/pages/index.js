dead3961c55c7bb86c2cf04bc0b7747f06d5ebb9
ydrea <ydrea.wrld@gmail.com>
2022-11-21 09:28:32 +0100

    migrate

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,8 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-import { twoDecimals } from "../utils/format";
-import { API_URL, fromImageToUrl } from "../utils/urls";
 // import { faFontAwesome } from "@fortawesome/free-regular-svg-icons";
 // import {faEuro} from '@fortawesome/free-regular-svg-icons'
 
@@ -16,36 +12,6 @@ export default function Home({ products }) {
           content="locally grown, organic & affordable meals"
         />
       </Head>
-
-      {products.map((product) => (
-        <div className={styles.product}>
-          <Link href={`/products/${product.id}`}>
-            <a>
-              <div className={styles.product__Rows}>
-                <div className={styles.product__ColImg}>
-                  <img src={fromImageToUrl(product.image)} />
-                </div>
-                <div className={styles.product__Col}>
-                  {product.name} &euro;{twoDecimals(product.price)}
-                </div>
-              </div>
-            </a>
-          </Link>
-        </div>
-      ))}
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const product_res = await fetch(`${API_URL}/products/`);
-
-  console.log(product_res);
-  const products = await product_res.json();
-  console.log(products);
-  return {
-    props: {
-      products,
-    },
-  };
 }
