39b4b41c2de82ab4b748dbd73fc06b8937f0f358
ydrea <ydrea.wrld@gmail.com>
2022-12-21 12:41:10 +0100

    better

--- next/pages/about.js
+++ next/pages/about.js
@@ -8,7 +8,9 @@ const about = () => {
         {" "}
         White Tree is a web portal (B2B) and a mobile app (B2C), aiming to
         identify and support restaurants and catering businesses that offer
-        locally sourced, organically grown, and/or affordable menu items.
+        locally sourced, organically grown, and/or affordable menu items, as
+        well as specific dietary choices: vegetarian, vegan, gluten-free, keto,
+        paleo etc.
       </p>
     </div>
   );
